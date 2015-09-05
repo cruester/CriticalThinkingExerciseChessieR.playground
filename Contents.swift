a8733e5213d35bb3a0f93d5b4c3e271aea6d704f
cruester <chessie.ruester@sbcglobal.net>
2015-09-05 10:53:41 -0500

    CriticalThinkingExerciseChessieR
    Apple picture playground

---
+++ Contents.swift
@@ -0,0 +1,156 @@
+
+//This displays an apple picture
+
+import UIKit
+
+var w = "\u{25a1}" //w stands for white, as in white space
+var b = "\u{25a0}" //b stands for black, as in black square
+let B = "\u{25a0}" //B also stands for black, but this is constant 
+
+/* declaring and assigning value to each line of my grid pic */
+
+let r1 = w + w + w + w + w + w + w + w + w + w + w + w +
+        w + B + B + w + w + w + w + w + w + w
+
+let r2 = w + w + w + w + w + w + w + w + w + w + w + w +
+    B + B + B + w + w + w + w + w + w + w
+
+let r3 = w + w + w + w + w + w + w + w + w + w + w + B + B +
+    B + w + w + w + w + w + w + w + w
+
+let r4 = w + w + w + w + w + w + w + w + w + w + w + B + B +
+    B + w + w + w + w + w + w + w + w
+
+let r5 = w + w + w + w + w + w + w + w + w + w + B + B +
+    w + w + w + w + w + w + w + w + w + w
+
+let r6 = w + w + w + w + w + w + B + B + w + w + B + w + w +
+        w + B + B + w + w + w + w + w + w
+
+var r7 = w + w + w + w + B + B + B + B + B + w + B + w + B +
+    B + B + B + b + b + w + w + w + w
+
+var r8 = w + w + w + B + B + B + B + B + B + B + B + B + B +
+        B + B + B + b + b + b + w + w + w
+
+var r9 = w + w + B + B + B + B + B + B + B + B + B + B + B +
+    B + B + B + B + B + B + B + w + w
+
+var r10 = w + B + B + B + B + B + B + B + B + B + B + B + B +
+    B + B + B + B + B + B + B + B + w
+
+var r11 = B + B + B + B + B + B + B + B + B + B + B + B + B +
+    B + B + B + B + B + B + B + B + B
+
+let r12 = B + B + B + B + B + B + B + B + B + B + B + B + B +
+    B + B + B + B + B + B + B + B + B
+
+let r13 = B + B + B + B + B + B + B + B + B + B + B + B + B +
+    B + B + B + B + B + B + B + B + B
+
+let r14 = B + B + B + B + B + B + B + B + B + B + B + B + B +
+    B + B + B + B + B + B + B + B + B
+
+let r15 = B + B + B + B + B + B + B + B + B + B + B + B + B +
+    B + B + B + B + B + B + B + B + B
+
+let r16 = w + B + B + B + B + B + B + B + B + B + B + B + B +
+    B + B + B + B + B + B + B + B + w
+
+let r17 = w + w + B + B + B + B + B + B + B + B + B + B + B +
+    B + B + B + B + B + B + B + w + w
+
+let r18 = w + w + B + B + B + B + B + B + B + B + B + B + B +
+    B + B + B + B + B + B + w + w + w
+
+let r19 = w + w + w + B + B + B + B + B + B + B + B + B + B +
+    B + B + B + B + B + B + w + w + w
+
+let r20 = w + w + w + w + B + B + B + B + B + B + B + B + B +
+    B + B + B + B + B + w + w + w + w
+
+let r21 = w + w + w + w + B + B + B + B + B + B + B + B + B +
+    B + B + B + B + B + w + w + w + w
+
+let r22 = w + w + w + w + w + w + B + B + B + w + w + w + w + B
+        + B + B + w + w + w + w + w + w
+
+
+
+println(r1)
+println(r2)
+println(r3)
+println(r4)
+println(r5)
+println(r6)
+println(r7)
+println(r8)
+println(r9)
+println(r10)
+println(r11)
+println(r12)
+println(r13)
+println(r14)
+println(r15)
+println(r16)
+println(r17)
+println(r18)
+println(r19)
+println(r20)
+println(r21)
+println(r22)
+
+println("") //Creating space between my pictures
+
+b = "\u{25a1}" // Changing the value of a previous black square
+
+
+/* xCode did not put the new variable value in these statements
+    unless they were reassigned */
+
+r7 = w + w + w + w + B + B + B + B + B + w + B + w + B +
+    B + B + B + B + b + w + w + w + w
+
+
+r8 = w + w + w + B + B + B + B + B + B + B + B + B + B +
+    B + B + B + b + b + b + w + w + w
+
+r9 = w + w + B + B + B + B + B + B + B + B + B + B + B +
+    B + B + B + b + b + b + w + w + w
+
+r10 = w + B + B + B + B + B + B + B + B + B + B + B + B +
+    B + B + B + B + B + b + b + b + w
+
+r11 = B + B + B + B + B + B + B + B + B + B + B + B + B +
+    B + B + B + B + B + B + B + B + B
+
+
+
+
+
+
+
+println(r1)
+println(r2)
+println(r3)
+println(r4)
+println(r5)
+println(r6)
+println(r7)
+println(r8)
+println(r9)
+println(r10)
+println(r11)
+println(r12)
+println(r13)
+println(r14)
+println(r15)
+println(r16)
+println(r17)
+println(r18)
+println(r19)
+println(r20)
+println(r21)
+println(r22)
+
+
